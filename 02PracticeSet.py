7add678c382afb08e1bfd3201b8ddab6cef144ea
kk0707singh <kk0707singh@gmail.com>
2025-09-02 18:03:12 +0530

    practice set 4

--- 02PracticeSet.py
+++ 02PracticeSet.py
@@ -29,5 +29,22 @@
 
 # QUESTION 3
 
-name = "krishna is   a  good boy"
-print(name.find("  "))
+# name = "krishna is   a  good boy"
+# print(name.find("  "))
+
+#===================CHAPTER 4 PRACTICE SET======================
+
+fruits = []
+fruit1 = input("Enter the Fruit name: ")
+fruits.append(fruit1)
+fruit2 = input("Enter the Fruit name: ")
+fruits.append(fruit2)
+fruit3 = input("Enter the Fruit name: ")
+fruits.append(fruit3)
+fruit4 = input("Enter the Fruit name: ")
+fruits.append(fruit4)
+fruit5 = input("Enter the Fruit name: ")
+fruits.append(fruit5)
+fruit6 = input("Enter the Fruit name: ")
+fruits.append(fruit6)
+print(fruits)
